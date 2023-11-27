9a436c0e5dac292079c17f58e7a69d7c0ae56f3c
phil <zhangsphilip@gmail.com>
2023-11-26 21:12:27 -0800

    implementing user class

--- app/src/main/java/com/example/cell_identifier/SignUpActivity.kt
+++ app/src/main/java/com/example/cell_identifier/SignUpActivity.kt
@@ -11,6 +11,7 @@ class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var fireBaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,29 +24,40 @@ class SignUpActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
-        binding.button.setOnClickListener{
+
+        binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
+            val firstName = binding.firstNameEt.text.toString()
+            val lastName = binding.lastNameEt.text.toString()
+            val bio = binding.bioEt.text.toString()
+            val age = binding.ageEt.text.toString().toIntOrNull() ?: 0
+            val school = binding.schoolEt.text.toString()
+            val role = binding.roleEt.text.toString()
+            val year = binding.yearEt.text.toString().toIntOrNull() ?: 0
+            val field = binding.fieldEt.text.toString()
 
-            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
-                if (pass == confirmPass){
+            if (pass == confirmPass) {
+                // Create a User instance with user input
+                val user = User(firstName, lastName, bio, age, school, role, year, field)
 
-                    fireBaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
-                        if(it.isSuccessful){
+                // Attempt to create a new user using Firebase Authentication
+                fireBaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            // If successful, navigate to the Sign In screen
                             val intent = Intent(this, SignInActivity::class.java)
                             startActivity(intent)
-                        }else{
+                        } else {
+                            // If there's an error, display a Toast with the error message
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
-                }else{
-                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
-                }
-            }else{
-                Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
+            } else {
+                // Display a Toast if password and confirm password do not match
+                Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 }
