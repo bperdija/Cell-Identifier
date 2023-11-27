b87129f5a3ed5d313ec06ea4cae54958a0f275c6
victory obanye <iobanye@sfu.ca>
2023-11-26 23:03:50 -0800

    user can now add in their profile information upon sign up and they can view their profile information within the app

--- app/src/main/java/com/example/cell_identifier/ProfileFragment.kt
+++ app/src/main/java/com/example/cell_identifier/ProfileFragment.kt
@@ -10,8 +10,11 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -25,7 +28,18 @@ class ProfileFragment : Fragment() {
     private lateinit var saveButton: Button
     private lateinit var fName: TextView
     private lateinit var lName: TextView
+
+    private lateinit var fullName: TextView
+    private lateinit var email: TextView
     private lateinit var bio: TextView
+    private lateinit var age: TextView
+    private lateinit var school: TextView
+    private lateinit var role: TextView
+    private lateinit var year: TextView
+    private lateinit var field: TextView
+
+    private lateinit var uid: String
+    private lateinit var user: User
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,18 +47,26 @@ class ProfileFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
+        fullName = view.findViewById(R.id.fullNameEt)
+        email = view.findViewById(R.id.emailEt)
+        bio = view.findViewById(R.id.bioEt)
+        age = view.findViewById(R.id.ageEt)
+        school = view.findViewById(R.id.schoolEt)
+        role = view.findViewById(R.id.roleEt)
+        year = view.findViewById(R.id.yearEt)
+        field = view.findViewById(R.id.fieldEt)
+        saveButton = view.findViewById(R.id.submit_button)
+
         // Initialize Firebase Auth, Database, etc.
         auth = FirebaseAuth.getInstance()
-        val uid = auth.currentUser?.uid
+        uid = auth.currentUser?.uid.toString()
         databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-
-        saveButton = view.findViewById(R.id.saveBtn)
-        fName = view.findViewById(R.id.etFirstName)
-        lName = view.findViewById(R.id.etLastName)
-        bio = view.findViewById(R.id.etBio)
+        if (uid.isNotEmpty()) {
+            getUserData()
+        }
 
         saveButton.setOnClickListener {
-            //saveProfile(uid)
+            //updateProfile(uid)
         }
 
         // Set default image URI
@@ -53,22 +75,52 @@ class ProfileFragment : Fragment() {
         return view
     }
 
-//    private fun saveProfile(uid: String?) {
-//        val firstName = fName.text.toString()
-//        val lastName = lName.text.toString()
-//        val bio = bio.text.toString()
-//
-//        val user = User(firstName, lastName, bio, age, school, role, year, field)
-//        if (uid != null) {
-//            databaseReference.child(uid).setValue(user).addOnCompleteListener {
-//                if (it.isSuccessful) {
-//                    uploadProfilePic()
-//                } else {
-//                    Toast.makeText(context, "Failed to update profile", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//    }
+    private fun getUserData() {
+        databaseReference.child(uid).addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                user = snapshot.getValue(User::class.java)!!
+                fullName.setText(user.firstName + " " + user.lastName)
+                email.setText(user.email )
+                bio.setText(user.bio )
+                age.setText(user.age.toString() )
+                school.setText(user.school )
+                role.setText(user.role )
+                year.setText(user.year.toString() )
+                field.setText(user.field )
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun updateProfile(uid: String?) {
+        val firstName = fName.text.toString()
+        val lastName = lName.text.toString()
+        val email = email.text.toString()
+        val bio = bio.text.toString()
+        val age = age.text.toString().toIntOrNull() ?: 0
+        val school = school.text.toString()
+        val role = role.text.toString()
+        val year = year.text.toString().toIntOrNull() ?: 0
+        val field = field.text.toString()
+
+
+        val user = User(firstName, lastName, email, bio, age, school, role, year, field)
+        if (uid != null) {
+            databaseReference.child(uid).setValue(user).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    uploadProfilePic()
+                } else {
+                    Toast.makeText(context, "Failed to update profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     private fun uploadProfilePic() {
         storageReference = FirebaseStorage.getInstance().getReference("Users/${auth.currentUser?.uid}")
