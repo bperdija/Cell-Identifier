76bdcbb75e3bc4295bc4db58b712c3e7e31136c5
victory obanye <iobanye@sfu.ca>
2023-11-26 19:38:48 -0800

    working on it

---
+++ app/src/main/java/com/example/cell_identifier/User.kt
@@ -0,0 +1,3 @@
+package com.example.cell_identifier
+
+data class User(var firstName: String, var lastName: String, var bio: String, var age: Int, var school: String, var role: String, var year: Int, var field: String)
